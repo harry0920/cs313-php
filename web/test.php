48c5b58fc18a0c9017f0543e3ab33afd0406c020
harry0920 <vas13008@byui.edu>
2018-05-01 16:42:48 -0600

    Test DIvs

--- web/test.php
+++ web/test.php
@@ -2,7 +2,19 @@
 <html lang="en">
     
     <body>
-        PHPTEST FILE.
+        <?php
+
+                for($i = 0; $i < 10; $i++){
+
+                    if($i%2 != 0) {
+                        echo "<div id=$i style='color:red;'> DIV $i</div>";
+                    }
+                    else{
+                        echo "<div id=$i> DIV $i </div>";
+                    }
+                }
+        
+        ?>
     </body>
     
 </html>
