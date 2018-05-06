7e4291f6cb12ee7d78a609a7c45a4992862c4dce
harry0920 <vas13008@byui.edu>
2018-05-06 00:11:24 -0600

    Index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            CS313 Home Page - Harry Vashisht. 
+        </title>
+    </head>
+    <body>
+        <h1>
+            Harry Vashisht's Home Page.
+        </h1>
+        <a href="#">Assignments</a>
+    </body>
+</html>
