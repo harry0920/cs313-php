15cd2c20fe22410ce141c49a892a5f798fe928c6
harry0920 <vas13008@byui.edu>
2018-05-06 01:22:03 -0600

    image test

--- web/about.php
+++ web/about.php
@@ -16,7 +16,7 @@
     
         
         <div class="polaroid">
-        <img src="img.jpg" alt="image" style="width:100%">
+        <img src="img.jpg" alt="image" style="width:50%">
         <div class="container">
             <p>I am a CIT major with a Minor in CS. I love to play soccer and watch it too. My favorite team is Real Madrid. Apart from soccer I am a pretty outdoors person too, hiking, swimming, climbing are some of my other hobbies. Once I graduate I would try and get a job that will give me alot of experience in Web Developement that eventually I will be able to open my own firm.</p>
         </div>
