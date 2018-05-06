5508df24209a26f99cd2bba3c911137b48e187e8
harry0920 <vas13008@byui.edu>
2018-05-06 00:58:27 -0600

    nav

--- web/nav.php
+++ web/nav.php
@@ -10,11 +10,20 @@
         <li>
             <a href="#">Portfolio ￬</a>
             <ul class="hidden">
-                <li><a href="#">Photography</a></li>
-                <li><a href="#">Web & User Interface Design</a></li>
-                <li><a href="#">Illustration</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#">Category</a></li>
             </ul>
         </li>
-        <li><a href="#">News</a></li>
+        <li>
+            <a href="#">Assignments</a>
+            <ul class="hidden">
+                <li><a href="#">Assignment1</a></li>
+                <li><a href="#">Assignment2</a></li>
+                 <li><a href="#">Assignment3</a></li>
+                 <li><a href="#">Assignment4</a></li>
+                 <li><a href="#">Assignment5</a></li>
+            </ul>
+        </li>
         <li><a href="#">Contact</a></li>
 </ul>
