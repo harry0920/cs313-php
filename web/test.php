27657b29b719378b7d38e6cab884e34f2834836e
harry0920 <vas13008@byui.edu>
2018-05-08 15:24:00 -0600

    Final push before week3

--- web/test.php
+++ web/test.php
@@ -4,16 +4,20 @@
     <body>
         <?php
 
+
+                if(ifset($URL["username"])){
+                    echo "Welcome $url['username']";
+                }
                 for($i = 0; $i < 10; $i++){
 
-                    if($i%2 != 0) {
+                    if($i%2 == 0) {
                         echo "<div id=$i style='color:red;'> DIV $i</div>";
                     }
                     else{
                         echo "<div id=$i> DIV $i </div>";
                     }
                 }
-        
+
         ?>
     </body>
     
