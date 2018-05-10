6636c625fca79ce6fa6a7efe83679140e0ed11a2
harry0920 <vas13008@byui.edu>
2018-05-10 16:05:22 -0600

    sessions

---
+++ web/week03/teach/session.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if(isset($_SESSION["Counter"])){
+    $_SESSION["Counter"]++;
+}
+else{
+    $_SESSION["Counter"] = 1;
+}
+
+$counter = $_SESSION["Counter"];
+?>
+<html>
+<body>
+<?php
+
+    echo "Counter: $Counter";
+
+?>
+</body>
+</html>
+
+
