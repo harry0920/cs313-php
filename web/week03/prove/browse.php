8193571dc232d593632649335adb85a1b4af51f8
harry0920 <vas13008@byui.edu>
2018-05-12 20:18:30 -0600

    Browse 9

--- web/week03/prove/browse.php
+++ web/week03/prove/browse.php
@@ -22,8 +22,57 @@
                 <button>+</button>
             </div>
 
-            <input type="submit" value="Submit Answers">
+            <label for="shoe2">Nike Shoe 2</label>
+            <div>
+                <img src="images/shoe2.jpg" alt="shoe2">
+                <button>+</button>
+            </div>
 
+            <label for="shoe3">Nike Shoe 3</label>
+            <div>
+                <img src="images/shoe3.jpg" alt="shoe3">
+                <button>+</button>
+            </div>
+
+            <label for="shoe4">Nike Shoe 4</label>
+            <div>
+                <img src="images/shoe4.jpg" alt="shoe4">
+                <button>+</button>
+            </div>
+
+            <label for="shoe5">Nike Shoe 5</label>
+            <div>
+                <img src="images/shoe5.jpg" alt="shoe5">
+                <button>+</button>
+            </div>
+
+            <label for="shoe6">Nike Shoe 6</label>
+            <div>
+                <img src="images/shoe6.jpg" alt="shoe6">
+                <button>+</button>
+            </div>
+
+            <label for="shoe7">Nike Shoe 7</label>
+            <div>
+                <img src="images/shoe7.jpg" alt="shoe7">
+                <button>+</button>
+            </div>
+
+            <label for="shoe8">Nike Shoe 8</label>
+            <div>
+                <img src="images/shoe8.jpg" alt="shoe8">
+                <button>+</button>
+            </div>
+
+            <label for="shoe9">Nike Shoe 9</label>
+            <div>
+                <img src="images/shoe9.jpg" alt="shoe9">
+                <button>+</button>
+            </div>
+
+
+
+            <input type="submit" value="Submit Answers">
 
         </form>
     </body>
