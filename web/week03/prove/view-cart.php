eff407c8119ab23adc0faf9a346d7f3cd60d62a1
harry0920 <vas13008@byui.edu>
2018-05-12 19:26:52 -0600

    Commit Through php storm

---
+++ web/week03/prove/view-cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            CS313 Home Page - Harry Vashisht
+        </title>   
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <?php
+        include '../../nav.php';
+        ?>
+        <h1>
+            Harry Vashisht's Home Page.
+        </h1>
+    </body>
+</html>
