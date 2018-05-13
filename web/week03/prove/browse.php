1ee9aed0f9686f94e912130fb2623e621a56bee0
harry0920 <vas13008@byui.edu>
2018-05-12 20:13:10 -0600

    Browse 1

--- web/week03/prove/browse.php
+++ web/week03/prove/browse.php
@@ -6,12 +6,25 @@
     </title>
     <link rel="stylesheet" type="text/css" href="../../styles/style.css">
 </head>
-<body>
-<?php
-include '../../nav.php';
-?>
-<h1>
-    Harry Vashisht's Home Page.
-</h1>
-</body>
+    <body>
+        <?php
+        include '../../nav.php';
+        ?>
+        <h1>
+            Harry Vashisht's Home Page.
+        </h1>
+        <form method="POST" action="results.php">
+            <p>Choose Item you want to buy: </p>
+
+            <label for="shoe1">Nike Shoe 1</label>
+            <div>
+                <img src="images/shoe1.jpg" alt="shoe1">
+                <button>+</button>
+            </div>
+
+            <input type="submit" value="Submit Answers">
+
+
+        </form>
+    </body>
 </html>
