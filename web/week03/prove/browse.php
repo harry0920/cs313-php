07c8e14fb81386a3689c251cb73f7fb619214e1e
harry0920 <vas13008@byui.edu>
2018-05-13 01:53:54 -0600

    Browse loop

--- web/week03/prove/browse.php
+++ web/week03/prove/browse.php
@@ -27,12 +27,19 @@ $products = array("shoe1", "shoe2", "shoe3", "shoe4", "shoe5", "shoe6", "shoe7",
         <?php
         for ($i=0; $i< count($products); $i++) {
 
-            echo "<label for='".$products[i]."'>".
+            /**echo "<label for='".$products[i]."'>".
                 "<div>".
                 "<img src='images/".$products[i].".jpg' alt='".$products[i]."'>" .
                 " <button onclick='addshoe('".$products[i]."')' class='w3-circle w3-green' >+</button>" .
                 "</div>".
                 "</label>";
+**/
+
+            echo "<label for=\"$products[i]\">Nike Shoe 1</label>
+            <div>
+                <img src=\"images/$products[i].jpg\" alt=\"$products[i]\">
+                <button onclick=\"addshoe('$products[i]')\" class=\"w3-circle w3-green\" >+</button>
+            </div>";
 
 
         }
