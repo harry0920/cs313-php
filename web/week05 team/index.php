04b7d098cc9d6f8f4e2b4724de5dc8aa9ec98e39
harry0920 <vas13008@byui.edu>
2018-05-22 16:00:03 -0600

    Team week 052

--- web/week05 team/index.php
+++ web/week05 team/index.php
@@ -4,6 +4,7 @@
        <title>First page</title>
    </head>
    <body>
+   <h1>Scripture Resources</h1>
       <?php
       $dbUrl = getenv('DATABASE_URL');
 
@@ -18,10 +19,9 @@
       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      foreach ($db->query('SELECT book, verse FROM scriptures') as $row)
+      foreach ($db->query('SELECT * FROM scriptures') as $row)
       {
-        echo 'user: ' . $row['book'];
-        echo ' password: ' . $row['verse'];
+        echo "<b>".$row['book']." ".$row['chapter'].":".$row['verse']." - </b>"."\"".$row['content']."\"";
         echo '<br/>';
       }
 
