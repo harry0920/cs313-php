b0609e31f043d4a7b26de26a56fc00e80d9f762e
harry0920 <vas13008@byui.edu>
2018-05-29 00:57:59 -0600

    index.php

--- web/nav.php
+++ web/nav.php
@@ -4,11 +4,29 @@
             <a href="week02/about.php">About</a>
         </li>
         <li>
-            <a href="#">Portfolio ￬</a>
+            <a href="#">Categories ￬</a>
             <ul class="hidden">
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Reviews</a></li>
-                <li><a href="#">Category</a></li>
+                <?php
+
+                    $dbUrl = getenv('DATABASE_URL');
+
+              $dbopts = parse_url($dbUrl);
+
+              $dbHost = $dbopts["host"];
+              $dbPort = $dbopts["port"];
+              $dbUser = $dbopts["user"];
+              $dbPassword = $dbopts["pass"];
+              $dbName = ltrim($dbopts["path"],'/');
+
+              $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+              $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              foreach ($db->query('SELECT * FROM categories') as $row)
+              {
+                  echo "<li><a href=\"category.php/id=".$row['id']."\">".$row['categoryName']."</a></li>";
+              }
+                ?>
+
             </ul>
         </li>
         <li>
@@ -16,9 +34,9 @@
             <ul class="hidden">
                 <li><a href="#">Assignment1</a></li>
                 <li><a href="#">Assignment2</a></li>
-                 <li><a href="week03/prove/browse.php">Assignment3</a></li>
+                 <li><a href="~/week03/prove/browse.php">Assignment3</a></li>
                  <li><a href="#">Assignment4</a></li>
-                 <li><a href="week05/prove/index.php">Assignment5</a></li>
+                 <li><a href="~/week05/prove/index.php">Assignment5</a></li>
             </ul>
         </li>
         <li><a href="contact.php">Contact</a></li>
