d2cfaddcb83be52f8dae7ba9b9b81e6a72ce9a9a
harry0920 <vas13008@byui.edu>
2018-05-29 16:35:36 -0600

    Week 06 team

--- web/week06/index.php
+++ web/week06/index.php
@@ -29,20 +29,19 @@
           $verse = $_POST['verse'];
           $content = $_POST['content'];
 
-          echo $book.$chapter.$verse.$content;
+       //   echo $book.$chapter.$verse.$content;
 
           $sql =  'INSERT INTO scriptures (book,chapter,verse,content) VALUES (:book, :chapter, :verse,:content)';
 
-          echo 'After sql';
+
 
           $stmt = $db->prepare($sql);
-          echo 'After prepare';
+
           $stmt->bindValue(':book', $book, PDO::PARAM_STR);
           $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-          $stmt->bindValue(':book', $verse, PDO::PARAM_INT);
-          $stmt->bindValue(':book', $content, PDO::PARAM_STR);
+          $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+          $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 
-          echo 'After bind';
 
           $stmt->execute();
 
