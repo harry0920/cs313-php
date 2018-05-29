a52fe1a12c693e01c18e1b30cc0e683045180b31
harry0920 <vas13008@byui.edu>
2018-05-28 22:13:15 -0600

    Prove week 05 commit

---
+++ web/week05/prove/index.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Shoes - Main Page
+    </title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    <body>
+        <?php
+        include '../../nav.php';
+        ?>
+        <div class="mainDiv">
+
+        <?php
+
+        /**
+        for ($i=0; $i< count($products); $i++)
+        {
+            $j = $i+1;
+            $y = $amounts[$i] + 20;
+
+            echo "<figure class=\"shoes\">"
+                ."<img src=\"images$products[$i].jpg\" alt=\"$names[$i]\">"
+                ."<figcaption>"
+                ."<h3>$names[$i]</h3>"
+                ."<p>Feel the latest Design!</p>"
+                ."<div class=\"price\"><s>$y</s>$amounts[$i]</div>"
+                ."</figcaption><i class=\"ion-android-cart\"></i>"
+                ."<a href=\"?add=$i\"></a>"
+                ."</figure>";
+        }
+         *
+         **/
+        ?>
+        </div>
+
+        <form class="browseform" action="view-cart.php">
+            <input type="submit" value="Shopping Cart">
+        </form>
+    </body>
+
+    <script>
+
+    </script>
+</html>
