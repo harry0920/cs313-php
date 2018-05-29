983f054372cbc0dc61b5ac6fc778d24977d60c1e
harry0920 <vas13008@byui.edu>
2018-05-29 00:40:58 -0600

    index.php

--- web/week05/prove/index.php
+++ web/week05/prove/index.php
@@ -21,7 +21,28 @@ session_start();
 
         <?php
 
-        /**
+              $dbUrl = getenv('DATABASE_URL');
+
+              $dbopts = parse_url($dbUrl);
+
+              $dbHost = $dbopts["host"];
+              $dbPort = $dbopts["port"];
+              $dbUser = $dbopts["user"];
+              $dbPassword = $dbopts["pass"];
+              $dbName = ltrim($dbopts["path"],'/');
+
+              $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+              $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              foreach ($db->query('SELECT * FROM inventory') as $row)
+              {
+                echo "<b>".$row['name'].:".$row['stock']";
+                echo '<br/>';
+              }
+
+
+
+            /**
         for ($i=0; $i< count($products); $i++)
         {
             $j = $i+1;
